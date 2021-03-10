1a438e79b8635194bd8951f967367d15c5b42fe1
joawa10 <joawa10@yahoo.com>
2021-03-11 07:25:49 +0800

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TipsComponent } from './tips/tips.component';
+import { ButtonsComponent } from './buttons/buttons.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TipsComponent,
+    ButtonsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
